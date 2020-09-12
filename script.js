793815f0db110b8a3d9f2d015f5b5fa2a092f30a
Jada Arnett <jada.arnett@gmail.com>
2020-09-11 20:49:05 -0400

    retrieved data from local storage, it persists when page is refreshed

--- script.js
+++ script.js
@@ -137,3 +137,10 @@ saveButton.on("click", function() {
 
 $("#9am").val(localStorage.getItem("9am"));
 $("#10am").val(localStorage.getItem("10am"));
+$("#11am").val(localStorage.getItem("11am"));
+$("#12pm").val(localStorage.getItem("12pm"));
+$("#1pm").val(localStorage.getItem("1pm"));
+$("#2pm").val(localStorage.getItem("2pm"));
+$("#3pm").val(localStorage.getItem("3pm"));
+$("#4pm").val(localStorage.getItem("4pm"));
+$("#5pm").val(localStorage.getItem("5pm"));
