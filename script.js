1841db55181fa923d46be182725005aab25ca697
Jada Arnett <jada.arnett@gmail.com>
2020-09-12 16:20:48 -0400

    removed columns from added classes

--- script.js
+++ script.js
@@ -34,78 +34,77 @@ $("#currentDay").append(currentDay);
 //var to set up the current time variable
 var currentTime = moment().hour();
 
-
 //  if else statements to get the color to update depending on the time
 if (currentTime > nineAm) {
-  $("#9am").addClass("past col-sm-8");
+  $("#9am").addClass("past");
 } else if (currentTime == nineAm) {
-  $("#9am").addClass("present col-sm-8");
+  $("#9am").addClass("present");
 } else if (currentTime < nineAm) {
-  $("#9am").addClass("future col-sm-8");
+  $("#9am").addClass("future");
 }
 
 if (currentTime > tenAm) {
-  $("#10am").addClass("past col-sm-8");
+  $("#10am").addClass("past  ");
 } else if (currentTime == tenAm) {
-  $("#10am").addClass("present col-sm-8");
+  $("#10am").addClass("present  ");
 } else if (currentTime < tenAm) {
-  $("#10am").addClass("future col-sm-8");
+  $("#10am").addClass("future  ");
 }
 
 if (currentTime > elevenAm) {
-  $("#11am").addClass("past col-sm-8");
+  $("#11am").addClass("past  ");
 } else if (currentTime == elevenAm) {
-  $("#11am").addClass("present col-sm-8");
+  $("#11am").addClass("present  ");
 } else if (currentTime < elevenAm) {
-  $("#11am").addClass("future col-sm-8");
+  $("#11am").addClass("future  ");
 }
 
 if (currentTime > twelvePm) {
-  $("#12pm").addClass("past col-sm-8");
+  $("#12pm").addClass("past  ");
 } else if (currentTime == twelvePm) {
-  $("#12pm").addClass("present col-sm-8");
+  $("#12pm").addClass("present  ");
 } else if (currentTime < twelvePm) {
-  $("#12pm").addClass("future col-sm-8");
+  $("#12pm").addClass("future  ");
 }
 
 if (currentTime > onePm) {
-  $("#1pm").addClass("past col-sm-8");
+  $("#1pm").addClass("past  ");
 } else if (currentTime == onePm) {
-  $("#1pm").addClass("present col-sm-8");
+  $("#1pm").addClass("present  ");
 } else if (currentTime < onePm) {
-  $("#1pm").addClass("future col-sm-8");
+  $("#1pm").addClass("future  ");
 }
 
 if (currentTime > twoPm) {
-  $("#2pm").addClass("past col-sm-8");
+  $("#2pm").addClass("past  ");
 } else if (currentTime == twoPm) {
-  $("#2pm").addClass("present col-sm-8");
+  $("#2pm").addClass("present  ");
 } else if (currentTime < twoPm) {
-  $("#2pm").addClass("future col-sm-8");
+  $("#2pm").addClass("future  ");
 }
 
 if (currentTime > threePm) {
-  $("#3pm").addClass("past col-sm-8");
+  $("#3pm").addClass("past  ");
 } else if (currentTime == threePm) {
-  $("#3pm").addClass("present col-sm-8");
+  $("#3pm").addClass("present  ");
 } else if (currentTime < threePm) {
-  $("#3pm").addClass("future col-sm-8");
+  $("#3pm").addClass("future  ");
 }
 
 if (currentTime > fourPm) {
-  $("#4pm").addClass("past col-sm-8");
+  $("#4pm").addClass("past  ");
 } else if (currentTime == fourPm) {
-  $("#4pm").addClass("present col-sm-8");
+  $("#4pm").addClass("present  ");
 } else if (currentTime < fourPm) {
-  $("#4pm").addClass("future col-sm-8");
+  $("#4pm").addClass("future  ");
 }
 
 if (currentTime > fivePm) {
-  $("#5pm").addClass("past col-sm-8");
+  $("#5pm").addClass("past  ");
 } else if (currentTime == fivePm) {
-  $("#5pm").addClass("present col-sm-8");
+  $("#5pm").addClass("present  ");
 } else if (currentTime < fivePm) {
-  $("#5pm").addClass("future col-sm-8");
+  $("#5pm").addClass("future  ");
 }
 
 // save button event listener to track which save button is clicked
